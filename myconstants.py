1f70fa641562ce0016a1f9a904b9d24624a50dbb
pjtat <pjtat.jk@gmail.com>
2024-09-12 19:12:46 -0600

    Created the initial python script to pull boxscore information from ESPN API

---
+++ myconstants.py
@@ -0,0 +1,97 @@
+
+# ESPN League Information 
+league_id = "963482"
+espn_cookies = {"swid": "{8834B8AF-D2A6-40D5-8AA6-4BC7CAC0A700}",
+    "espn_s2": "AECN%2F04Cjt8Xx%2BT3WyMoqYBGSZ%2FYhUZgBXUNM5tBl5Dl4676mVwngMhGN4kkgjc9706OdWLGL%2FpFhEi4I%2FBXodkNBDj%2B4Nf5Z3KIonTgsxBf8oedaKYrhVNcjVA%2Fimeu6oL79MsHTB0%2BAFZrFEE9puexOrRFMdAqQe9z%2F6BWGBtwmLVsXH6LS1R5KMIlDBn%2Fy%2BETpAY3EAZ%2FWCkFSK71h8WpNbFx8nxWyrH0MjZEyGpnLPeJgKSf3QlyxkbbiCUwtg3gbqKNuscljjm9ey2J9gnB"}
+
+# Request Information 
+headers = {
+ 'Connection': 'keep-alive',
+ 'Accept': 'application/json, text/plain, */*',
+ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+}
+
+# List of Owners
+# ** Need to figure out a better way of pulling these 
+listOfOwners = {
+    1: {
+        "prefName": "Nick",
+        "display": True,
+        "ownerID": "{049CC4E6-F81E-4FE4-9CC4-E6F81E8FE4E1}"
+    },
+    2: {
+        "prefName": "Brian",
+        "display": True,
+        "ownerID": "{3E11F700-BE0C-4EB3-8787-605D4723B083}"
+    },
+    3: {
+        "prefName": "Jack",
+        "display": True,
+        "ownerID": "{4918E72B-8495-4B5D-B3FE-7DD7DF8588F7}"
+    },
+    4: {
+        "prefName": "Brett",
+        "display": True,
+        "ownerID": "{5DF83CAC-7A14-4BCD-B83C-AC7A14BBCDC5}"
+    },
+    5: {
+        "prefName": "Chris",
+        "display": True,
+        "ownerID": "{7B012F7E-7BF8-42D2-812F-7E7BF8F2D26E}"
+    },
+    6: {
+        "prefName": "Pj",
+        "display": True,
+        "ownerID": "{B6CAEBA9-F067-42E3-BA15-D9CEB1886017}"
+    },
+    7: {
+        "prefName": "Dob",
+        "display": True,
+        "ownerID": "{BEAF742F-B7A6-4F3D-83FE-89F609804AA2}"
+    },
+    8: {
+        "prefName": "Fitz",
+        "display": True,
+        "ownerID": "{C2E5066A-6E64-4D79-BE8B-88DA422D743E}"
+    },
+    9: {
+        "prefName": "Justin",
+        "display": True,
+        "ownerID": "{CC9F4C14-1535-4BCA-B067-5306573C289C}"
+    },
+    10: {
+        "prefName": "Boyle",
+        "display": True,
+        "ownerID": "{CD3CE4A6-333C-44A7-8145-6876AEBDE7AB}"
+    },
+    11: {
+        "prefName": "Deegs",
+        "display": False,
+        "ownerID": "{E7D43363-C371-4189-8BC0-06EA06D3938D}"
+    },
+    12: {
+        "prefName": "Colin",
+        "display": False,
+        "ownerID": "{BA760152-C166-41FF-92B3-0A62F0C22B6D}"
+    },
+    13: {
+        "prefName": "St. Thomas",
+        "display": False,
+        "ownerID": "{9812C7E1-6528-410F-93CC-108337B6E740}"
+    },
+    14: {
+        "prefName": "Fissinger",
+        "display": False,
+        "ownerID": "{7C53EBF6-EF35-45EC-93EB-F6EF35C5ECEA}"
+    },
+    15: {
+        "prefName": "Hunter",
+        "display": False,
+        "ownerID": "{9359EB30-9F86-4B2F-99EB-309F86EB2F16}"
+    },
+    16: {
+        "prefName": "Cole",
+        "display": False,
+        "ownerID": "{74C9DD2F-BDED-453E-89DD-2FBDED053E7D}"
+    }
+}
